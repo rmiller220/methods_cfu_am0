98ba8126c520f33f612c4193270e1a2751f3940c
Reilly Miller <reillyjmiller31@gmail.com>
2023-01-05 14:12:58 -0700

    fix define and add hw

--- define_and_id.rb
+++ define_and_id.rb
@@ -14,30 +14,30 @@ p "Hello World".include?("Hello")
 
 # The include method is called on the string object "Hello World". 
 # The job of include is to ask whether or not the string includes the "Hello" anywhere in the string.
-# The return value responds with a true
+# The return value is true
 
 p "Hello World".end_with?("Hello")
 
 # The end_with method is called on the string object "Hello World". 
 # The job of end_with is to ask whether or not the string ends with "Hello". 
-# The return value will print false.
+# The return value is false.
 
 "Hello World".end_with?("rld")
 
 # The end_with method is called on the string object "Hello World".
 # The job of end_with is to ask whether or not the string ends with "rld". 
-# The return value will print true.
+# The return value is true.
 
 
 p 12.even?
 
 # The even method is called on the integer 12. 
 # The job of even is to ask whether or not the integer is even. 
-# The return value will print true
+# The return value is true
 
 
 p 18.next
 
 # The next method is called on the integer 18. 
 # The job of next is to ask what the next number will be in the sequence. 
-# The return value will print 19.
+# The return value is 19.
